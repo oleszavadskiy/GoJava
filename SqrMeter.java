7bf2508204871373237fbdd8d5fe0788acb7dcf5
oleszavadskiy <oleszavadskiy@gmail.com>
2016-04-17 15:50:33 +0300

    Домашнее задание Модуль 4/1
    module4/1

---
+++ SqrMeter.java
@@ -0,0 +1,19 @@
+package SquareMeter;
+
+/**
+ * Created by Олесь on 16.04.2016.
+ */
+public class SqrMeter {
+    public static void main(String args[]){
+    int sideA = 10;
+    int sideB = 10;
+    int sideC = 15;
+    double p = (sideA + sideB + sideC) / 2;
+
+      double s = (p * (p - sideA) * (p - sideB) * (p - sideC));
+    double S = Math.sqrt(s);
+
+
+    System.out.println(S);
+}
+}
